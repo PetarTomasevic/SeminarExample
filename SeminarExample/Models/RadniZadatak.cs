295bd5cc669c4a9d00bc0ef59e076982cf0b0d76
Petar <petar.tomasevic@magnanimous.info>
2023-03-09 19:05:46 +0100

    Add project files.

---
+++ SeminarExample/Models/RadniZadatak.cs
@@ -0,0 +1,13 @@
+﻿namespace SeminarExample.Models
+{
+    public class RadniZadatak
+    {
+        public int Id { get; set; }
+        public string ToDo { get; set; }
+        public string OpisPosla { get; set; }
+        public string RadnikIme { get; set; }
+        public string RadnikPrezime { get; set; }
+        public DateTime? Datum { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
